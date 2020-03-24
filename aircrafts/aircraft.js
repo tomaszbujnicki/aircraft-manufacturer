870c2cbe21d92e433a6767e843865984e1bdc503
Tomasz Bujnicki <tomek.bujnicki@gmail.com>
2020-03-24 12:34:59 +0100

    aircraftsToInvent= []

--- aircrafts/aircraft.js
+++ aircrafts/aircraft.js
@@ -36,7 +36,47 @@ const aircrafts = [
 		workers: 0
 	}
 ]
-
+aircraftsToInvent= [
+	{
+		id: 3,
+		name: "Porshe",
+		img: "aircrafts/img/aeroplane2.svg",
+		parts: 100,
+		corePrice: 150000,
+		price: 150000,
+		passengers: 110,
+		quantity: 0,
+		productionStage: 0,
+		workers: 0,
+		inventionPoints: 50
+	},
+	{
+		id: 4,
+		name: "Ferrari",
+		img: "aircrafts/img/airplane1.svg",
+		parts: 10,
+		corePrice: 15000,
+		price: 15000,
+		passengers: 110,
+		quantity: 0,
+		productionStage: 0,
+		workers: 0,
+		inventionPoints: 100
+	},
+	{
+		id: 5,
+		name: "Mercedes",
+		img: "aircrafts/img/aeroplane3.svg",
+		parts: 99,
+		corePrice: 105000,
+		price: 105000,
+		passengers: 70,
+		quantity: 0,
+		productionStage: 0,
+		workers: 0,
+		inventionPoints: 150
+	}
+]
 
 function createElementAircraft(aircraft){
 	if (!aircraft) return ;
@@ -131,3 +171,4 @@ function raiseAircraftPrice(){
 	}
 
 }
+
