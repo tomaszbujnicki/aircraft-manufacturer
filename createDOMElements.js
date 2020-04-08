f02e7d448b8b54feaba88d5d8a6067d8f762bd92
Tomasz Bujnicki <tomek.bujnicki@gmail.com>
2020-04-08 14:33:26 +0200

    Move creating employee elements to html

--- createDOMElements.js
+++ createDOMElements.js
@@ -2,8 +2,6 @@ function createElements(){
 
     for (const aircraft of aircraftArray) createElementAircraft(aircraft);
 
-    for (const employee of employeeList) createElementEmployee(employee);
-
     for (const aircraft of aircraftToInvent) createElementAircraftToInvent(aircraft);
 
     for (const loan of loanList) createElementLoan(loan);
--- js/navigation/openEmployeeDescription.js
+++ js/navigation/openEmployeeDescription.js
@@ -1,13 +1,17 @@
 document.getElementById("closeEmployeeDescriptionBtn").addEventListener("click", () => closeEmployeeDescription());
+for (let i = 0; i < 5; i++){
+    const button = document.getElementById("employeeCard" + i);
+    button.addEventListener("click", () => openEmployeeDescription( i ))
+}
 
 
-function openEmployeeDescription( id ) {
+function openEmployeeDescription( i ) {
 
     const allCards = document.querySelectorAll(".employeeDescription");
     allCards.forEach(element => element.classList.add("hide"));   
     
     document.getElementById("employeeDescriptionContainer").classList.remove("hide");
-	document.getElementById("employeeDescription" + id).classList.remove("hide");
+	document.getElementById("employeeDescription" + i).classList.remove("hide");
 }
 
 function closeEmployeeDescription() {
